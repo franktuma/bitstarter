91296cd2eb21083d69948b3a8bb2e174986cd16e
Frank Tuma <frank.tuma@gmail.com>
2013-07-12 08:01:10 +0000

    finished part3... ugly

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -37,7 +38,7 @@ var assertFileExists = function(infile) {
 };
 
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+    return cheerio.load(htmlfile);
 };
 
 var loadChecks = function(checksfile) {
@@ -65,10 +66,28 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+    if (program.url) {
+      console.log('procesing url: \'' + program.url + '\'');
+      rest.get(program.url).on('complete', function(result) {
+        if (result instanceof Error) {
+          console.log('Error: ' + result.message);
+          process.exit(1);
+        } else {
+          var checkJson = checkHtmlFile(result, program.checks);
+          var outJson = JSON.stringify(checkJson, null, 4);
+          console.log(outJson);
+        }
+      });
+    }
+    else if (program.file) {
+      console.log('processing file: \'' + program.file + '\'');
+      var html = fs.readFileSync(program.file);
+      var checkJson = checkHtmlFile(html, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
+  } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
